7a361a166280201e39994938e3f44737c32e7b24
VladislavShtefan <123403369+VladislavShtefan@users.noreply.github.com>
2023-03-16 14:08:13 +0400

    Сайт к 16.03
    Не получилось закинуть файлы через desktop. На сайте нет стилизации ползунка, эффекты при наведении на элементы (кнопки и т.д.), так же расставил focus для клавиатуры. Пока скинул сюда, но продолжу делать.

---
+++ main.js
@@ -0,0 +1,19 @@
+const popup = document.querySelector('.popup');
+const btn = document.querySelector('.btn__js');
+const closePopup = document.querySelector('.modal__close');
+/* const formName = document.activeElement('.form__name');*/
+
+btn.addEventListener('click', () => {
+    popup.classList.add('open');
+});
+
+closePopup.addEventListener('click', () => {
+    popup.classList.remove('open');
+});
+
+/* window.addEventListener("keydown", function(e) {
+    if (e.keyCode === "Escape" && popup.classList.contains('open')) {
+        popup.classList.remove('open');
+        btn.focus();
+    }
+}); */
