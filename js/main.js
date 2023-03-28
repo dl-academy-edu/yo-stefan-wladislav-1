287ed5f4444c75525248d74ed9260aec2281a582
VladislavShtefan <corvus338228@gmail.com>
2023-03-28 23:11:16 +0400

    Учебный сайт (повторно)
    Залил все новые файлы. Вроде все заработало.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const popup = document.querySelector('.popup');
 const btn = document.querySelector('.btn__js');
+const closePopup = document.querySelector('.pouring');
 const closePopup = document.querySelector('.modal__close');
-/* const formName = document.activeElement('.form__name');*/
+const formName = document.activeElement('.form__name');
 
 btn.addEventListener('click', () => {
     popup.classList.add('open');
@@ -11,9 +12,14 @@ closePopup.addEventListener('click', () => {
     popup.classList.remove('open');
 });
 
-/* window.addEventListener("keydown", function(e) {
+closePopup.addEventListener('click', () => {
+    popup.classList.remove('close');
+});
+
+
+window.addEventListener("keydown", function(e) {
     if (e.keyCode === "Escape" && popup.classList.contains('open')) {
         popup.classList.remove('open');
         btn.focus();
     }
-}); */
+});
